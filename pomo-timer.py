a09441005ad8eb07b00a6fc236afa7315d1f759a
Miranty1 <mirantymatthew@gmail.com>
2023-02-20 10:51:19 +1000

    added sound notification

--- pomo-timer.py
+++ pomo-timer.py
@@ -16,8 +16,9 @@ Sfont = pygame.font.Font('assets/pong.ttf',25)
 FONT = pygame.font.Font("assets/pong.ttf", 120)
 timer_text = FONT.render("25:00", True, "white")
 timer_text_rect = timer_text.get_rect(center=(WIDTH/2, HEIGHT/2-25))
- 
 
+sound_sfx = pygame.mixer.Sound('assets/sound.mp3')
+ 
 
 START_STOP_BUTTON = Button(image = WHITE_BUTTON, pos = (350, 500), text_input ="START", font = Sfont,   base_color = 'black', hovering_color = 'pink')
 POMO_BUTTON = Button(image = WHITE_BUTTON, pos = (WIDTH/2-250, HEIGHT/2-300), text_input ="POMO", font = font,   base_color = 'black', hovering_color = 'pink')
@@ -25,9 +26,9 @@ SHORTB_BUTTON = Button(image = WHITE_BUTTON, pos = (WIDTH/2, HEIGHT/2-300), text
 LONGB_BUTTON = Button(image = WHITE_BUTTON, pos = (WIDTH/2+250, HEIGHT/2-300), text_input ="LONG BREAK", font = font,   base_color = 'black', hovering_color = 'pink')
 
  
-POMODORO_LENGTH = 2# 1500 secs / 25 mins
-SHORT_BREAK_LENGTH =4# 300 secs / 5 mins
-LONG_BREAK_LENGTH = 5# 900 secs / 15 mins
+POMODORO_LENGTH = 1500# 1500 secs / 25 mins
+SHORT_BREAK_LENGTH =300# 300 secs / 5 mins
+LONG_BREAK_LENGTH = 900# 900 secs / 15 mins
 
 cycle = 1
 
@@ -94,6 +95,7 @@ while True:
 
 
     if cycle == 0 and current_seconds <=0:
+            sound_sfx.play()
             print(cycle)
             cycle += 1
             started = False
@@ -102,6 +104,7 @@ while True:
             START_STOP_BUTTON.text = pygame.font.Font("assets/pong.ttf", 20).render(
                                                 START_STOP_BUTTON.text_input, True, START_STOP_BUTTON.base_color)
     if cycle == 1 and current_seconds <=0:
+        sound_sfx.play()
         print(cycle)
         cycle += 1
         started = False
@@ -110,6 +113,7 @@ while True:
         START_STOP_BUTTON.text = pygame.font.Font("assets/pong.ttf", 20).render(
                                             START_STOP_BUTTON.text_input, True, START_STOP_BUTTON.base_color)
     if cycle == 2 and current_seconds <= 0:
+        sound_sfx.play()
         print(cycle)
         cycle += 1
         started = False
@@ -118,6 +122,7 @@ while True:
         START_STOP_BUTTON.text = pygame.font.Font("assets/pong.ttf", 20).render(
                                             START_STOP_BUTTON.text_input, True, START_STOP_BUTTON.base_color)
     if cycle == 3 and current_seconds <= 0:
+        sound_sfx.play()
         print(cycle)
         cycle += 1
         started = False
@@ -126,6 +131,7 @@ while True:
         START_STOP_BUTTON.text = pygame.font.Font("assets/pong.ttf", 20).render(
                                             START_STOP_BUTTON.text_input, True, START_STOP_BUTTON.base_color)
     if cycle == 4 and current_seconds <= 0:
+        sound_sfx.play()
         print(cycle)
         cycle += 1
         started = False
@@ -134,6 +140,7 @@ while True:
         START_STOP_BUTTON.text = pygame.font.Font("assets/pong.ttf", 20).render(
                                             START_STOP_BUTTON.text_input, True, START_STOP_BUTTON.base_color)
     if cycle == 5 and current_seconds <= 0:
+        sound_sfx.play()
         print(cycle)
         cycle -= 5
         started = False
